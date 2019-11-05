8e92dc0924d2ee695fef38751a567b7084e72a91
B-Sireesha11 <57249212+B-Sireesha11@users.noreply.github.com>
2019-11-05 12:24:59 +0530

    Update Get_Request.java

--- src/test/java/com/api/testcases/Get_Request.java
+++ src/test/java/com/api/testcases/Get_Request.java
@@ -2,6 +2,10 @@ package com.api.testcases;
 
 
 public class Get_Request {
+	public  Get_Request() {
+		 System.out.println("constructor");
+	 }
+}
 	 public void get_request() {
 		 System.out.println("get request");
 	 }
